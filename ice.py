f877fb6e6385c16cb4f008616d28423376b8e060
Brandon Stafford <brandon@pingswept.org>
2025-11-28 11:27:15 -0500

    Update ice.py

--- ice.py
+++ ice.py
@@ -15,8 +15,8 @@ url = "https://archive-api.open-meteo.com/v1/archive"
 params = {
 	"latitude": 43.732,
 	"longitude": -71.5884,
-	"start_date": "2025-11-01",
-	"end_date": "2025-11-26",
+	"start_date": "2024-11-01",
+	"end_date": "2025-01-15",
 	"hourly": ["temperature_2m", "snow_depth"],
 }
 responses = openmeteo.weather_api(url, params=params)
@@ -39,8 +39,19 @@ hourly_data = {"date": pd.date_range(
 	inclusive = "left"
 )}
 
+def stefan(row):
+    if row["degree_days"] > 0:
+        return 0
+    else:
+        return 0.035 * (-1 * row["degree_days"]) ** 0.5
+
 hourly_data["temperature_2m"] = hourly_temperature_2m
 hourly_data["snow_depth"] = hourly_snow_depth
 
+hourly_data["degree_days"] = (hourly_data["temperature_2m"]/24).cumsum()
+
 hourly_dataframe = pd.DataFrame(data = hourly_data)
+hourly_dataframe["ice_thickness_m"] = hourly_dataframe.apply(stefan, axis=1)
+hourly_dataframe["ice_thickness_in"] = hourly_dataframe["ice_thickness_m"] * 39.37008
+
 print("\nHourly data\n", hourly_dataframe)
